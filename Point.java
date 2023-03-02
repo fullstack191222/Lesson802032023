e1ffb9594abd22fc556c6c946f83b83cddafdfe4
fullstack191222 <120251571+fullstack191222@users.noreply.github.com>
2023-03-02 21:21:22 +0200

    Create Point.java

---
+++ Point.java
@@ -0,0 +1,13 @@
+public class Point {
+    int axelX;//3
+    int axelY;//2
+
+    public Point(int axelX, int axelY) {
+        this.axelX = axelX;
+        this.axelY = axelY;
+    }
+
+    public void printSomething () {
+        System.out.println("something");
+    }
+}
